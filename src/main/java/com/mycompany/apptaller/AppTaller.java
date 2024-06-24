39bae93d6362d9ca73216cbd413221a865408089
Jarood <jaroodjimili@gmail.com>
2024-06-24 18:17:19 -0500

    iniciando proyecto

---
+++ src/main/java/com/mycompany/apptaller/AppTaller.java
@@ -0,0 +1,16 @@
+/*
+ * Click nbfs://nbhost/SystemFileSystem/Templates/Licenses/license-default.txt to change this license
+ */
+
+package com.mycompany.apptaller;
+
+/**
+ *
+ * @author JJAB
+ */
+public class AppTaller {
+
+    public static void main(String[] args) {
+        System.out.println("Hello World!");
+    }
+}
